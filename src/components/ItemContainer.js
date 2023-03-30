23c15dbf806070c06aba4de7c4d00e3d39eeb092
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-30 10:19:53 -0400

    mapDispatchToPropos versatil

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'
 
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item - {props.item}</h2>
+      <button onClick={props.buyItem}>Buy Items</button>
     </div>
   )
 }
@@ -15,4 +17,10 @@ const mapStateToPtops = (state, ownProps) => {
       item: itemState
     }
 }
-export default connect(mapStateToPtops)(ItemContainer)
+const mapDispatchToPtops = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake ? () => dispatch(buyCake()) : () => dispatch(buyIceCream())
+    return {
+      buyItem: dispatchFunction
+    }
+}
+export default connect(mapStateToPtops, mapDispatchToPtops)(ItemContainer)
